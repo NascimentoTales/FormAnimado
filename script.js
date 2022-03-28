46cb6b38db0a88772e8bd6a4c7562fc8b69e05a4
NascimentoTales <nascimentosantostales@gmail.com>
2022-03-28 13:53:49 -0300

    Testando5

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
+document.querySelector("body").style.overflow = "hidden"
 
 let squares = document.querySelector(".squares");
 
 for(i=0; i<10; i++){
     
     let li = document.createElement("li");
+    
+    const random = (min, max) => Math.random() * (min - max) + max;
 
-    const random = (max, min) => Math.random() * (max - min) + min;
-
-    let size = random(120, 20) ;
-    let position = random(99, 1);
+    let size = Math.floor(random(10, 120));
+    let position = random(1, 99);
     let delay = random(5, 0.1);
-    let duration = random(12, 3);
+    let duration = random(3, 12);
     
     li.style.width = `${size}px`;
     li.style.height = `${size}px`;
     li.style.bottom = `-${size}px`;
 
     li.style.left = `${position}%`;
+
     li.style.animationDelay = `${delay}s`;
     li.style.animationDuration = `${duration}s`
-    li.style.animationTimingFunction = `cubic-bezier(${Math.random()})`
+    li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`
 
 
     li.style.position = `${position}vw`
@@ -29,4 +31,41 @@ for(i=0; i<10; i++){
 
 
 
+let container = document.querySelector(".container")
 
+document.querySelector("button").addEventListener("click", event =>{
+    event.preventDefault()
+
+    const fields = [...document.querySelectorAll("input")]
+
+    fields.forEach(element => {
+
+        if (element.value === "") {
+            container.classList.add("validate")
+        }
+    })
+
+    const validate = document.querySelector(".validate")
+    console.log(validate);
+    if (validate) {
+        validate.addEventListener("animationend", event =>{
+            if (event.animationName === "nono") {
+                container.classList.remove("validate")
+
+            }
+        })
+    }else{
+        container.classList.add("down")
+    }
+
+});
+
+
+container.addEventListener("animationstart", () => {
+    document.querySelector("body").style.overflow = "hidden"
+})
+container.addEventListener("animationend", (event) => {
+     if (event.animationName === "down") {
+         container.style.display = "none";
+     }
+})
